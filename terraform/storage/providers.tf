0938828ae2ccea95f2ee7fff3b1251d983480bbf
Dylan McCarthy <dylan.mccarthy@azenix.com.au>
2022-10-10 08:23:37 +1100

    fixing providers file

--- terraform/storage/providers.tf
+++ terraform/storage/providers.tf
@@ -4,16 +4,16 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 3.0.2"
     }
-    backend "azurerm" {
-        resource_group_name = "terraform-state"
-        storage_account_name = "terraformstate"
-        container_name = "terraform-state"
-        key = "{var.environment}.tfstate"
-        use_azuread_auth = true
-    }
+  }
+  backend "azurerm" {
+    resource_group_name = "terraform-state"
+    storage_account_name = "terraformstate"
+    container_name = "terraform-state"
+    key = "{var.environment}.tfstate"
+    use_azuread_auth = true
   }
 
-    required_version = ">= 1.1.0"
+  required_version = ">= 1.1.0"
 }
 
 provider "azurerm" {
